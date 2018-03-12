7acf3aedef79d123c8c65d84cf148be74a709326
andrewrgoss <andrewrgoss@gmail.com>
2018-03-12 15:20:36 -0400

    customize report fields, associated database query to add sample value extracts

--- data_dictionary.py
+++ data_dictionary.py
@@ -13,7 +13,7 @@ curs = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
 def get_dictionary():
     q = """
-    select t1.nspname as schema, t3.description, count(*) as count
+    select t1.nspname as schema, count(*) as count
     from pg_namespace t1
     join information_schema.tables t2 on t1.nspname = t2.table_schema
     left outer join pg_description t3 on t1.oid = t3.objoid
@@ -30,7 +30,7 @@ def get_dictionary():
         schema_name = schema['schema']
 
         q = """
-        select table_name as table, t3.description
+        select table_name as table
         from information_schema.tables t1
         join pg_class t2 on (table_name = relname)
         left outer join pg_description t3 on (t2.oid = objoid and objsubid = 0)
@@ -46,12 +46,21 @@ def get_dictionary():
             table_name = table['table']
 
             q = """
-            select column_name as column, data_type, is_nullable, t3.description
-            from information_schema.columns t1
-            join pg_class t2 on (t1.table_name = t2.relname)
-            left outer join pg_description t3 on (t2.oid = t3.objoid and t3.objsubid = t1.ordinal_position)
-            where table_schema = '{schema_name}'
-              and table_name = '{table_name}'
+            with samples as (
+              select table_schema, 
+                     table_name, 
+                     query_to_xml(format('select * from %I.%I limit 1', table_schema, table_name), true, true, '') 
+                     as sample_row
+              from information_schema.tables 
+              where table_schema = '{schema_name}'
+            )
+            select c.column_name as column, c.data_type,
+                case when s.sample_row is document 
+                    then (xpath('/row/'||column_name||'/text()', s.sample_row))[1]::text 
+                else null end as sample_value
+            from information_schema.columns c 
+              join samples s on (s.table_schema, s.table_name) = (c.table_schema, c.table_name)
+            where c.table_name = '{table_name}'
             order by ordinal_position
             """.format(**vars())
 
